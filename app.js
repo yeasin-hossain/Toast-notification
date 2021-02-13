d5b5119ca42ccb496488f4a3d87bec5df158ced5
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-02-13 21:05:27 +0600

    toast notification demo

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('btn');
+const container = document.getElementById('container');
+
+btn.addEventListener('click', () =>{
+    createNotification();
+});
+
+function createNotification(){
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+
+    notif.innerText = 'This is Awesome lorem kand gfszd';
+    container.appendChild(notif);
+
+    setTimeout (()=>{
+        notif.remove();
+    },3000);
+}
